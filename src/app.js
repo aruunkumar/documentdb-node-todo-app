ef42cd6ebef84cfa558e3bf56492a384133337f3
Aruun Kumar <aruunkumar@gmail.com>
2019-10-01 22:07:29 -0400

    updated

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.get('/', taskList.showTasks.bind(taskList));
 app.post('/addtask', taskList.addTask.bind(taskList));
 app.post('/completetask', taskList.completeTask.bind(taskList));
 
-// catch 404 and forward to error handler
+// catch 404 and forward to the error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
